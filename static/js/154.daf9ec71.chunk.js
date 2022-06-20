d837b155c999f3bf12f83213788f34ced7b76ad4
blksy <79380937+blksy@users.noreply.github.com>
2022-06-20 20:46:56 +0000

    Deploying to gh-pages from @ blksy/goit-react-hw-05-movies@36e766adb69988da9912bf1d4cbf8375dc715e13 🚀

---
+++ static/js/154.daf9ec71.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[154],{154:function(e,t,n){n.r(t),n.d(t,{default:function(){return _}});var u="NotFoundPage_title__uDivS",i=n(184);function _(){return(0,i.jsx)("h1",{className:u,children:"404 PAGE NOT FOUND"})}}}]);
+//# sourceMappingURL=154.daf9ec71.chunk.js.map
